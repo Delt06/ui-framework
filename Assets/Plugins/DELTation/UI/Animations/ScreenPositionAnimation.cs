7d6bafef8f1f5be88394965fbff1a3662c9acc50
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-12-27 23:36:04 +0300

    Add screen position animation

---
+++ Assets/Plugins/DELTation/UI/Animations/ScreenPositionAnimation.cs
@@ -0,0 +1,27 @@
+﻿using DELTation.UI.Animations.Tweeners.Properties;
+using DELTation.UI.Tweeners;
+using UnityEngine;
+
+namespace DELTation.UI.Animations
+{
+	public sealed class ScreenPositionAnimation : ScreenAnimation<Vector3, Vector3>
+	{
+		[SerializeField] private bool _multiplyValuesByRectSize = false;
+
+		protected override ScreenTweener<Vector3> CreateTweener(Vector3 openState, Vector3 closedState)
+		{
+			var rectTransform = (RectTransform) transform;
+
+			if (_multiplyValuesByRectSize)
+			{
+				var rect = rectTransform.rect;
+				openState *= rect.size;
+				closedState *= rect.size;
+			}
+
+			return new LocalPositionScreenTweener(rectTransform, openState, closedState);
+		}
+
+		protected override Vector3 GetInitialState() => transform.localPosition;
+	}
+}
